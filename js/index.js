35e0a8bc86a10063b45eef3c6a04dadbd0c49a89
Ryan Sukale <ryansukale@gmail.com>
2013-12-11 11:00:19 -0500

    just added the one page scroll plugin sample

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	
+	$(".main-content").onepage_scroll({
+		sectionContainer: "section", 
+		easing: "ease",
+		animationTime: 1000,
+		pagination: true,
+		updateURL: false
+	});
+	
+});

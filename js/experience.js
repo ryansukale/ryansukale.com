d1faeafdd0507f50f531f5f892a7dab226c629ff
Ryan Sukale <ryansukale@gmail.com>
2014-01-13 00:10:28 -0500

    Using underscore to render job blocks

---
+++ js/experience.js
@@ -0,0 +1,35 @@
+$(function(){
+	
+	var tmplJobRow = _.template($('#_tmplJobRow').html());
+	
+	$.getJSON( "data/experience.json", function( jobs ) {
+		
+		_.each(jobs,function(element, index, list){
+				var jobRow = tmplJobRow(element);
+				$('.job-details').append(jobRow);
+		});
+		
+		var propertyColors = _.pluck(jobs,'property-color'),
+		$jobs = $('.job'),
+		bgSelector = '.start, .separator, .end, .role',
+		borderSelector = '.org-name',
+		orgNameSelector = '.org-name';
+		
+		_.each(propertyColors, function(color,index,list){
+			$(bgSelector,$jobs.eq(index)).css({
+				'background-color':color
+			});
+			
+			$(orgNameSelector,$jobs.eq(index)).css({
+				'color':color
+			});
+			
+			$(borderSelector,$jobs.eq(index)).css({
+				'border-color':color
+			});
+			
+		});
+	
+	});
+		
+});
